395aa5a037d393cbce4a55cacce5b972018da4c4
lamwaiben <344540720@qq.com>
2020-02-21 12:09:04 +0800

    [ADD]handwrit_generator启动器

---
+++ interview/handwrite/generator.js
@@ -0,0 +1,42 @@
+/* 
+    generator自动执行器, 类似 async/await
+*/
+
+function run(gen){
+    let g = gen()
+    function next(data){
+        let result = g.next(data)               // next(data)相当于 yeild表达式替换成一个值
+        if (result.done) return result.value
+        if (result.value instanceof Promise) {      // 处理异步函数
+            result.value.then(res => next(res))
+        } else if(typeof result.value == 'function') {  // 处理函数, 函数参数通过bind传
+            result.value(next)
+        } else {
+            next(result.value)      // 处理纯数据
+        }
+    }
+    return next()
+}
+
+
+// 测试
+function func(data, cb) {
+    console.log(data);
+    cb();
+}
+
+function *test() {
+    let c = yield 0
+    console.log(c)
+    let a = yield Promise.resolve(1)
+    console.log(a)
+    let b = yield Promise.resolve(2)
+    console.log(b);
+    yield func.bind(null, a + b);       // 同步函数通过bind传函数, 调用放在自动器中
+}
+
+run(test)
+// 0
+// 1
+// 2
+// 3
