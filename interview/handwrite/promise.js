301b8f55e04b19373d31e4edefadb374c311b312
lamwaiben <344540720@qq.com>
2020-02-21 17:42:34 +0800

    [ADD]handwrit_promise

---
+++ interview/handwrite/promise.js
@@ -0,0 +1,190 @@
+/* 
+    手写一个简单的promise(非promiseA+)
+    完整版可以参考: lamwaiben.github.io/code/doOneself/bPromise
+
+    简单版思路:
+    1. 由于promise是可以链式调用的, 也就是说调用.then()之后必须要返回一个newPromise, 所以then函数必须要返回一个newPromise
+    2. 因为newPromise的状态是随上一个oldPromise的执行状态而改变的, 也就是newPromise的resolve/reject的触发是在oldPromise执行它自己的resolve/reject,
+        所以oldPromise的resolve/reject最主要的功能就是执行newPromise提交的回调函数
+*/
+const _status = Symbol('status')
+const _value = Symbol('value')
+const PENDING = 1;
+const FULFILLED = 2;
+const REJECTED = 3;
+
+class BPromise {
+    constructor(func) {
+        this[_status] = PENDING;
+        this[_value] = undefined;
+        this.resolveQueue = [];
+        this.rejectQueue = [];
+
+        func(resolve.bind(this), reject.bind(this))
+
+        function resolve(val) {
+            if (this[_status] === PENDING) {
+                setTimeout(() => {
+                    this[_status] = FULFILLED
+                    this[_value] = val
+                    this.resolveQueue.forEach(cb => {
+                        cb(val)
+                    });
+                })
+            }
+        }
+
+        function reject(err) {
+            if (this[_status] === PENDING) {
+                setTimeout(() => {
+                    this[_status] = REJECTED
+                    this[_value] = err
+                    this.rejectQueue.forEach(cb => {
+                        cb(err)
+                    });
+                })
+            }
+        }
+    }
+
+    then(onResolve, onReject) {
+        // 先判断参数合法性, 如果then的参数不是函数时, 默认是一个值传递函数
+        onResolve = typeof onResolve == 'function' ? onResolve : val => val
+        onReject = typeof onReject == 'function' ? onReject : reason => { throw reason }
+
+        // 如果是Promise.resolve().then()
+        if (this[_status] == FULFILLED) {
+            return new BPromise((resolve, reject) => {
+                setTimeout(() => {
+                    try {
+                        let x = onResolve(this[_value])
+                        this.handleThenReturnPromise(x, resolve, reject)    // 这里控制返回的newPromise的状态
+                    } catch (error) {
+                        reject(error)
+                    }
+                })
+            })
+            // 如果是Promise.reject().then()
+        } else if (this[_status] == REJECTED) {
+            return new BPromise((resolve, reject) => {
+                setTimeout(() => {
+                    try {
+                        let x = onReject(this[_value])
+                        this.handleThenReturnPromise(x, resolve, reject)
+                    } catch (error) {
+                        reject(error)
+                    }
+                })
+            })
+            // 如果是new Promise((resolve, reject) => resolve).then(onResolve, onReject)
+        } else if (this[_status] == PENDING) {
+            return new BPromise((resolve, reject) => {
+                // 提供回调函数给oldPromise触发
+                this.resolveQueue.push((val) => {
+                    try {
+                        let x = onResolve(val)
+                        this.handleThenReturnPromise(x, resolve, reject)
+                    } catch (error) {
+                        reject(error)
+                    }
+                })
+
+                this.rejectQueue.push((val) => {
+                    try {
+                        let x = onReject(val)
+                        this.handleThenReturnPromise(x, resolve, reject)
+                    } catch (error) {
+                        reject(error)
+                    }
+                })
+            })
+        }
+    }
+
+    catch(onReject) {
+        return this.then(null, onReject)
+    }
+
+    handleThenReturnPromise(x, resolve, reject) {
+        // 这里其实可以做更多事情: 处理thenable对象, 返回一个新的promise对象
+        try {
+            // 判断返回值是否为then中是否返回了Promise对象, 如果是则递归
+            if (x instanceof BPromise) {
+                x.then(resolve)
+            } else {
+                resolve(x)
+            }
+        } catch (error) {
+            reject(error)
+        }
+    }
+}
+
+
+BPromise.resolve = function (val) {
+    return new BPromise(resolve => {
+        resolve(val)
+    })
+}
+
+BPromise.reject = function (err) {
+    return new BPromise(resolve, reject => {
+        reject(err)
+    })
+}
+
+BPromise.all = function (promises) {
+    return new Promise((resolve, reject) => {
+        try {
+            let result = []
+            let cnt = promises.length
+            for (let i = 0; i < promises.length; i++) {
+                promises[i].then(res => {
+                    result[i] = res
+                    if(--cnt === 0) resolve(result)
+                }, error => reject(error))
+            }
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
+BPromise.race = function (promises) {
+    return new Promise((resolve, reject) => {
+        try {
+            for (let i = 0; i < promises.length; i++) {
+                promises[i].then(resolve, reject)
+            }
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
+
+// 测试
+new BPromise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('setTimeout 1000ms')
+        resolve(1000)
+        // reject(1000)
+    }, 1000)
+}).then(
+    res => {
+        console.log('promise then res:', res)
+        return res
+    },
+    err => {
+        console.log('promise catch err:', err)
+        return -1000
+    }).then(
+        res => {
+            console.log('22222222222222222 promise then res:', res)
+            return res
+        },
+        err => {
+            console.log('22222222222222222 promise catch err:', err)
+            return -1000
+        }
+    )
