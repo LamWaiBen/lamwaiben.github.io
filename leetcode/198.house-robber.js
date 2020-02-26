8af7abe933d51658a0ff9e3e8d2c3770658ad8d7
lamwaiben <344540720@qq.com>
2020-02-26 19:06:35 +0800

    [ADD]leetcode_198

--- leetcode/198.house-robber.js
+++ leetcode/198.house-robber.js
@@ -31,7 +31,7 @@ var rob = function (nums) {
     dp[0] = nums[0]
     dp[1] = Math.max(dp[0], nums[1])
     for (let i = 2; i < nums.length; i++) {
-        dp[i] = Math.max(dp[i - 2] + nums[i], dp[i])
+        dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1])
     }
     return dp[nums.length - 1]
 };
