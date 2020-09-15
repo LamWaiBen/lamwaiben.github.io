f14a0911ece47d1c304809f8e1c1bbb977edd937
lamwaiben <344540720@qq.com>
2020-09-15 18:31:42 +0800

    [ADD]typescript

--- note/typescript/readme.md
+++ note/typescript/readme.md
@@ -1,5 +1,17 @@
 ## 记录用过的语法
 
+### 常见的约定字母
+- T Type泛型
+- K Key
+- V Number
+- E Element
+- P Parameter
+- R return
+- F Function
+- A Arg
+- S,U,V etc. - 2nd, 3rd, 4th types
+
+
 #### interface
 1. 继承多个interface
 使用逗号分隔
@@ -9,6 +21,15 @@ interface B {}
 interface AB extends A, B
 ```
 
+#### infer
+相当于占位符, 用于在类型声明中临时建立泛型关系
+```typescript
+type T1 = { name: string };
+type T2 = { age: number };
+  
+type UnionToIntersection<T> = T extends { a: (x: infer U) => void; b: (x: infer U) => void } ? U : never;
+type T3 = UnionToIntersection<{ a: (x: T1) => void; b: (x: T2) => void }>; // T1 & T2
+```
 
 #### 常见语法糖
 
@@ -37,4 +58,13 @@ type Partial<T> = {
 type Omit<T, K extends string | number | symbol> = { 
     [P in Exclude<keyof T, K>]: T[P]; 
 }
+```
+
+5. ReturnType, 把函数作为泛型, 取其返回类型作为类型
+```typescript
+type ReturnType<T extends (...args: any) => any> = T extends (...args: any) => infer R ? R : any
+```
+6. Parameters, 把函数作为泛型, 取其参数类型作为类型
+```typescript
+type Parameters<T extends (...args: any) => any> = T extends (...args: infer P) => any ? P : never 
 ```
---
+++ note/typescript/vuex.ts
@@ -0,0 +1,71 @@
+type GetMutations<Module> = Module extends { mutations: infer M} ? M: never
+
+// ts 4.1 新特性
+type AddPrefix<Prefix, Keys> = `${Prefix & string}/${Keys & string}`
+
+type GetSubModuleKeys<Module, Key> = Module extends { modules: infer SubModules }
+    ? AddPrefix<Key, GetMutations<SubModules>>
+    : never
+
+type GetModuleMutationsKeys<Module, Key> = AddPrefix<Key, keyof GetMutations<Module>> | GetSubModuleKeys<Module, Key>
+type GetModulesMutationsKeys<Modules> = {
+    [K in keyof Modules]: GetModuleMutationsKeys<Modules, K>
+}
+
+type Action<Mutations, Modules> = keyof Mutations | GetModulesMutationsKeys<Modules>
+
+type Store<Mutations, Modules> = {
+    dispatch(actions: Action<Mutations, Modules>) : void
+}
+
+type VuexOptions<Mutations, Modules> = {
+    mutations: Mutations,
+    modules: Modules
+}
+
+declare function Vuex<Mutations, Modules>(options: VuexOptions<Mutations, Modules>): Store<Mutations, Modules>
+const store = Vuex({
+    mutations: {
+        root(state, ) {},
+    },
+
+    modules: {
+        cart: {
+            mutations: {
+                sell(state) {
+
+                }
+            },
+            actions: {
+                sell(store) {
+
+                }
+            },
+            getter: {
+                stock(state) {
+
+                }
+            },
+        },
+        user: {
+            mutations: {
+                login(state){}
+            },
+            modules: {
+                admin: {
+                    mutations: {
+                        login(state){}
+                    }
+                }
+            }
+        }
+    }
+})
+
+// test
+store.dispatch('root')
+store.dispatch('cart/add')
+store.dispatch('user/login')
+store.dispatch('user/admin/login')
+
+
